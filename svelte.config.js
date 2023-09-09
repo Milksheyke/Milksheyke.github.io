bf02c1340cdff003c74829270b243ec662239444
Milksheyke <amilksheyke@gmail.com>
2023-09-09 01:42:55 -0400

    Add tailwind

--- svelte.config.js
+++ svelte.config.js
@@ -1,13 +1,15 @@
-import adapter from 'svelte-adapter-bun';
-import preprocess from 'svelte-preprocess'
+import adapter from "svelte-adapter-bun"
+import preprocess from "svelte-preprocess"
+import { vitePreprocess } from "@sveltejs/kit/vite"
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
   preprocess: preprocess(),
 
   kit: {
-		adapter: adapter(),
-	}
-};
+    adapter: adapter(),
+  },
+  preprocess: vitePreprocess(),
+}
 
-export default config;
+export default config
