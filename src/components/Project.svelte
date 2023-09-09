ce338bc2f0c18d6c139fe4da586a6d695e3a0c95
Milksheyke <amilksheyke@gmail.com>
2023-09-09 01:44:29 -0400

    Create simple project page

---
+++ src/components/Project.svelte
@@ -0,0 +1,19 @@
+<script>
+  export let title
+  export let description
+  export let link
+</script>
+
+<div class="project">
+  <h3>{title}</h3>
+  <p>{description}</p>
+  <a href={link} target="_blank">View Project</a>
+</div>
+
+<style>
+  .project {
+    border: 1px solid #ccc;
+    padding: 10px;
+    margin: 10px 0;
+  }
+</style>
